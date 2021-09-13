ac1221946da25fbcf1badab91cea9e35dfb93fc3
igor <marks.ihor@gmail.com>
2021-09-13 12:08:45 +0300

    name the routes

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,17 @@
 <?php
 
-Route::group(['as' => 'roles', 'prefix' => 'roles'], function () {
-    Route::get('/', [config('roles.routes.namespace'), 'index']);
-    Route::get('/{roleId}', [config('roles.routes.namespace'), 'show']);
-    Route::get('/{roleId}/permissions', [config('roles.routes.namespace'), 'rolePermissions']);
-    Route::get('/{roleId}/permissions_grouped', [config('roles.routes.namespace'), 'rolePermissionsGrouped']);
-    Route::post('/', [config('roles.routes.namespace'), 'store']);
-    Route::patch('/{roleId}', [config('roles.routes.namespace'), 'update']);
-    Route::delete('/{roleId}', [config('roles.routes.namespace'), 'delete']);
+Route::group(['as' => 'roles.', 'prefix' => 'roles'], function () {
+    Route::get('/', [config('roles.routes.namespace'), 'index'])->name('index');
+    Route::get('/{roleId}', [config('roles.routes.namespace'), 'show'])->name('show');
+    Route::get('/{roleId}/permissions', [config('roles.routes.namespace'), 'rolePermissions'])
+        ->name('permissions');
+    Route::get('/{roleId}/permissions_grouped', [config('roles.routes.namespace'), 'rolePermissionsGrouped'])
+        ->name('rolePermissionsGrouped');
+    Route::post('/', [config('roles.routes.namespace'), 'store'])->name('store');
+    Route::patch('/{roleId}', [config('roles.routes.namespace'), 'update'])->name('update');
+    Route::delete('/{roleId}', [config('roles.routes.namespace'), 'delete'])->name('delete');
     Route::patch('/{roleId}/permissions/{action}', [config('roles.routes.namespace'), 'updatePermissions'])
+        ->name('updatePermissions')
         ->where('action', '(attach|detach|update)');
 });
-Route::get('/permissions', [config('roles.routes.namespace'), 'permissions']);
+Route::get('/permissions', [config('roles.routes.namespace'), 'permissions'])->name('permissions');
